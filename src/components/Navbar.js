3bdf64fb1bed010bd08bea61904a5a6f8d088415
thapaRoyal <thaparoyal17@gmail.com>
2021-08-28 12:26:48 +0545

    finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,70 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export default function Navbar({ title }) {
+export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          {title}
-        </a>
+        <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                {props.aboutText}
+              </Link>
+            </li>
+          </ul>
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              onClick={props.toggleMode}
+              type="checkbox"
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable DarkMode
+            </label>
+          </div>
+        </div>
       </div>
     </nav>
   );
 }
 
-Navbar.propTypes = { title: PropTypes.string };
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired,
+};
 
 Navbar.defaultProps = {
-  title: "TextUtils",
+  title: "Set title here",
+  aboutText: "About",
 };
